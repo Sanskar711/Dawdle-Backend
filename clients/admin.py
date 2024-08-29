516ea1becfaea6da1fdad5aac889d5a568c38df6
Sanskar711 <sanskarcr71102@gmail.com>
2024-08-30 02:10:39 +0530

    added products column on admin usecases

--- clients/admin.py
+++ clients/admin.py
@@ -1,21 +1,30 @@
 from django.contrib import admin
-from .models import Client, Product, UseCase, Prospect, Meeting, QualifyingQuestion, Resource, IdealCustomerProfile, QualifyingQuestionResponse
-from .models import EmailRequest
+from .models import Client, Product, UseCase, Prospect, Meeting, QualifyingQuestion, Resource, IdealCustomerProfile, QualifyingQuestionResponse, EmailRequest
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'client')
     search_fields = ('name', 'client__name')
-    filter_horizontal = ('assigned_users',)  # Allow admin to assign users to products
+    filter_horizontal = ('assigned_users',)
 
 class ProspectAdmin(admin.ModelAdmin):
-    list_display = ('company_name', 'is_approved', 'geography', 'status')  # Display these fields in the admin list view
-    list_filter = ('is_approved', 'status', 'geography')  # Add filters for easy searching
-    search_fields = ('company_name', 'geography')  # Allow searching by company name and geography
+    list_display = ('company_name', 'is_approved', 'geography', 'status')
+    list_filter = ('is_approved', 'status', 'geography')
+    search_fields = ('company_name', 'geography')
+
+# Custom UseCaseAdmin to display linked products
+class UseCaseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'get_linked_products')
+
+    def get_linked_products(self, obj):
+        # Join product names into a comma-separated string
+        return ", ".join([product.name for product in obj.products.all()])
+    
+    get_linked_products.short_description = 'Linked Products'
 
 admin.site.register(Client)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(UseCase)
-admin.site.register(Prospect, ProspectAdmin)  # Register with the custom ProspectAdmin
+admin.site.register(UseCase, UseCaseAdmin)
+admin.site.register(Prospect, ProspectAdmin)
 admin.site.register(Meeting)
 admin.site.register(QualifyingQuestionResponse)
 admin.site.register(Resource)
