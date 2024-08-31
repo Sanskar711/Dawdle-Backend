da7d2ee35158efb9b3a579062c5691da8ad5f4ec
Sanskar711 <sanskarcr71102@gmail.com>
2024-08-31 18:04:35 +0530

    renamed qualifying question responses in meeting model

--- clients/models.py
+++ clients/models.py
@@ -164,7 +164,7 @@ class Meeting(models.Model):
     scheduled_at = models.DateTimeField()
     
     # Updated Field: Direct ManyToMany relationship
-    ualifying_question_responses = models.ManyToManyField(
+    qualifying_question_responses = models.ManyToManyField(
         QualifyingQuestionResponse,
         related_name='meetings'
     )
